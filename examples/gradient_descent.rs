a469c69f92aa68bb77804b67a75395ab68e08422
KoBruhh <atomoyuncu.yt@gmail.com>
2022-12-24 23:08:40 +0300

    Added some test and examples which uses gnuplot, added some more loss functions and started with linear regression model (still in examples directory tho)

--- examples/gradient_descent.rs
+++ examples/gradient_descent.rs
@@ -1,3 +1,26 @@
+extern crate csv;
+extern crate gnuplot;
+
+use gnuplot::Figure;
+
 fn main() {
-    println!("ananla oynamak istiyorum");
+    let mut x: Vec<f64> = Vec::new();
+    let mut y: Vec<f64> = Vec::new();
+    let mut fg = Figure::new();
+    let axes = fg.axes2d();
+    let mut rdr =
+        csv::Reader::from_path("/home/kobruh/github/mars/examples/example_data/test.csv").unwrap();
+    for result in rdr.records() {
+        let record = result.unwrap();
+        x.push(record[0].parse().unwrap());
+        y.push(record[1].parse().unwrap());
+        axes.points(
+            &x, // x-coordinates
+            &y, // y-coordinates
+            &[],
+        );
+    }
+
+    // axes.set_color(Color::RGB(1, 0, 0)); // Set the color to red}
+    fg.show().unwrap();
 }
---
+++ examples/mean_squared_error.rs
@@ -0,0 +1,16 @@
+// use std::ops::{Sub};
+
+pub fn mean_squared_error(act: &Vec<f64>, pred: &Vec<f64>) -> f64 {
+    let mut sum = 0.0; // -> initialize sum of every element
+    for (act, pred) in act.iter().zip(pred) {
+        sum += (act - pred).abs().powi(2);
+    }
+    sum / pred.len() as f64
+}
+
+fn main() {
+    let act = vec![1.1, 2., 1.7];
+    let pred = vec![1., 1.7, 1.5];
+    let result = mean_squared_error(&act, &pred);
+    print!("{result}")
+}
